8aae23773680096391a9b2c5359be070cb399a46
Rafael Misato <misatorafael@gmail.com>
2020-10-22 18:34:54 -0300

    Updates

---
+++ precache-manifest.2c425447e2fe126a07f2c785f367b26d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6803aca93eb21ef735c016c745f5e1d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b11b74f3eef91e93566",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "38e308203315f83ddf3c",
+    "url": "/static/js/2.930d12d1.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.930d12d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b11b74f3eef91e93566",
+    "url": "/static/js/main.16da97be.chunk.js"
+  },
+  {
+    "revision": "0283b8e122a1ae2b95c1",
+    "url": "/static/js/runtime-main.140e061d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
