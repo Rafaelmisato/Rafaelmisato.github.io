48d72f9382a8a9ae2a853bcc54675dbcd313283d
Rafael Misato <misatorafael@gmail.com>
2020-11-04 18:22:16 -0300

    implemented new features on work section

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ function TabNavigation() {
 }
 
 
-/****/
+/**form**/
 
 window.addEventListener("DOMContentLoaded", function() {
 
@@ -105,3 +105,21 @@ window.addEventListener('load', () => {
 	const tabNavigation = TabNavigation()
 	tabNavigation.init()
 })
+
+/**smile**/
+
+document.querySelector('body')
+.addEventListener('mousemove', eyeball)
+
+function eyeball(){
+	const eye = document.querySelectorAll('.eye');
+	eye.forEach((eye) => {
+		let x = (eye.getBoundingClientRect().left)
+		+ (eye.clientWidth / 2);
+		let y = (eye.getBoundingClientRect().top)
+		+ (eye.clientHeight / 2);
+		let radian = Math.atan2(event.pageX - x, event.pageY - y);
+		let rot = (radian * (180 / Math.PI) * -1) + 270;
+		eye.style.transform = "rotate("+ rot +"deg)"
+	})
+}
