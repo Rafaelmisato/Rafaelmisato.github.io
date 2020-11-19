78d22f5080f03a5658554a1495357ac3a842456a
Rafael Misato <misatorafael@gmail.com>
2020-11-19 15:09:30 -0300

    testing

--- js/main.js
+++ js/main.js
@@ -1,78 +1,78 @@
 /**loader**/
-document.onreadystatechange = function() { 
-	if (document.readyState !== "complete") { 
-			document.querySelector( 
-				"body").style.visibility = "hidden"; 
-			document.querySelector( 
-				"#loader").style.visibility = "visible"; 
-	} else { 
-			document.querySelector( 
-				"#loader").style.display = "none"; 
-			document.querySelector( 
-				"body").style.visibility = "visible"; 
-	} 
-}; 
+// document.onreadystatechange = function() { 
+// 	if (document.readyState !== "complete") { 
+// 			document.querySelector( 
+// 				"body").style.visibility = "hidden"; 
+// 			document.querySelector( 
+// 				"#loader").style.visibility = "visible"; 
+// 	} else { 
+// 			document.querySelector( 
+// 				"#loader").style.display = "none"; 
+// 			document.querySelector( 
+// 				"body").style.visibility = "visible"; 
+// 	} 
+// }; 
 
 /**menu scroll**/
-const menuItems = document.querySelectorAll('.menu-link');
+// const menuItems = document.querySelectorAll('.menu-link');
 
-menuItems.forEach(item => {
-	item.addEventListener('click', scrollToIdOnClick)
-})
+// menuItems.forEach(item => {
+// 	item.addEventListener('click', scrollToIdOnClick)
+// })
 
-function scrollToIdOnClick(event) {
-	event.preventDefault()
-	const element = event.target;
-	const to = getScrollTopByHref(event.target);
+// function scrollToIdOnClick(event) {
+// 	event.preventDefault()
+// 	const element = event.target;
+// 	const to = getScrollTopByHref(event.target);
 
-	scrollToPosition(to);
-}
+// 	scrollToPosition(to);
+// }
 
-function scrollToPosition(to) {
-	// window.scroll({
-	// 	top: to,
-	// 	behavior: "smooth",
-	// });
-	smoothScrollTo(0, to);
-}
+// function scrollToPosition(to) {
+// 	// window.scroll({
+// 	// 	top: to,
+// 	// 	behavior: "smooth",
+// 	// });
+// 	smoothScrollTo(0, to);
+// }
 
-function getScrollTopByHref(element) {
-	const id = element.getAttribute('href');
-	return document.querySelector(id).offsetTop;
-}
+// function getScrollTopByHref(element) {
+// 	const id = element.getAttribute('href');
+// 	return document.querySelector(id).offsetTop;
+// }
 
 
-/**
- * Smooth scroll animation
- * @param {int} endX: destination x coordinate
- * @param {int} endY: destination y coordinate
- * @param {int} duration: animation duration in ms
- */
-function smoothScrollTo(endX, endY, duration) {
-  const startX = window.scrollX || window.pageXOffset;
-  const startY = window.scrollY || window.pageYOffset;
-  const distanceX = endX - startX;
-  const distanceY = endY - startY;
-  const startTime = new Date().getTime();
+// /**
+//  * Smooth scroll animation
+//  * @param {int} endX: destination x coordinate
+//  * @param {int} endY: destination y coordinate
+//  * @param {int} duration: animation duration in ms
+//  */
+// function smoothScrollTo(endX, endY, duration) {
+//   const startX = window.scrollX || window.pageXOffset;
+//   const startY = window.scrollY || window.pageYOffset;
+//   const distanceX = endX - startX;
+//   const distanceY = endY - startY;
+//   const startTime = new Date().getTime();
 
-  duration = typeof duration !== 'undefined' ? duration : 600;
+//   duration = typeof duration !== 'undefined' ? duration : 600;
 
-  // Easing function
-  const easeInOutQuart = (time, from, distance, duration) => {
-    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
-    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
-  };
+//   // Easing function
+//   const easeInOutQuart = (time, from, distance, duration) => {
+//     if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+//     return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+//   };
 
-  const timer = setInterval(() => {
-    const time = new Date().getTime() - startTime;
-    const newX = easeInOutQuart(time, startX, distanceX, duration);
-    const newY = easeInOutQuart(time, startY, distanceY, duration);
-    if (time >= duration) {
-      clearInterval(timer);
-    }
-    window.scroll(newX, newY);
-  }, 1000 / 60); // 60 fps
-};
+//   const timer = setInterval(() => {
+//     const time = new Date().getTime() - startTime;
+//     const newX = easeInOutQuart(time, startX, distanceX, duration);
+//     const newY = easeInOutQuart(time, startY, distanceY, duration);
+//     if (time >= duration) {
+//       clearInterval(timer);
+//     }
+//     window.scroll(newX, newY);
+//   }, 1000 / 60); // 60 fps
+// };
 
 /**form**/
 window.addEventListener("DOMContentLoaded", function() {
